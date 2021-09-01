73c58e993eaa265f1ed1aa628990fd7ca2cca367
Alexander Upton <Alexander99W@ou.edu>
2021-09-01 11:22:57 -0500

    Add project files.

---
+++ WPF_Intro/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Intro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //lblOutput.Visibility = Visibility.Hidden;
+            lblOutput.Content = "";
+
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            int age = Convert.ToInt32(txtAge.Text);
+
+            DateTime today = DateTime.Now;
+            int birthyear = today.Year - age;
+
+            lblOutput.Content = $"You were born in {birthyear.ToString("G0")}";
+            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
